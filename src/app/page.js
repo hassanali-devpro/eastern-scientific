5f3a62cb9a892b148d0672ac6ad2f82a6ce13aaf
Syed Hassan Bukhari <hassanalishah0335>
2025-04-12 13:46:54 +0500

    google analytics Microsoft clarity

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,82 @@
-import Eastern from "./pages/home/page"
+import Head from 'next/head';
+import Script from 'next/script';
+import Eastern from "./pages/home/page";
 
 export default function Home() {
   return (
+    <>
+      <Head>
+        <meta name="p:domain_verify" content="4125685236e36cf545aeb34725c9bca4" />
+      </Head>
+
+      {/* Facebook Pixel */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1452203679077885');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      {/* Microsoft Clarity */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "qyr0x0ugm3");
+        `}
+      </Script>
+
+      {/* Google Tag Manager */}
+      <Script id="gtm-init" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MKTVLNDG');
+        `}
+      </Script>
+
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-905W0N41QZ"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-905W0N41QZ');
+        `}
+      </Script>
+
+      {/* GTM Noscript iframe */}
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+            <noscript>
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKTVLNDG"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            </noscript>
+          `,
+        }}
+      />
+
       <div className="">
         <Eastern />
       </div>
+    </>
   );
 }
